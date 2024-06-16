d1302c5b28293922240b23d53d6f882b171b9cd4
DylanRitchings <DylanRitchings1998@gmail.com>
2024-06-16 20:33:16 +0100

    mac

--- init.lua
+++ init.lua
@@ -1,45 +1,72 @@
--- Packer.nvim configuration
-vim.cmd [[packadd packer.nvim]]
+-- lazy nvim auto setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+--
 
-require('packer').startup(function()
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+vim.g.mapleader = " "
 
+require("lazy").setup({
   -- LSP Configuration & Plugins
-  use 'neovim/nvim-lspconfig'   -- Collection of configurations for built-in LSP client
-  use 'williamboman/mason.nvim' -- LSP server installer
-  use 'nvim-treesitter/nvim-treesitter'
-  use "elentok/format-on-save.nvim"
-  use 'mfussenegger/nvim-dap'
+  'neovim/nvim-lspconfig',   -- Collection of configurations for built-in LSP client
+  'williamboman/mason.nvim', -- LSP server installer
+  'nvim-treesitter/nvim-treesitter',
+  "elentok/format-on-save.nvim",
+  'mfussenegger/nvim-dap',
+  'folke/lazydev.nvim',
 
   -- Autocompletion plugins
-  use 'hrsh7th/nvim-cmp'              -- Autocompletion plugin
-  use 'hrsh7th/cmp-nvim-lsp'          -- LSP source for nvim-cmp
-  use 'hrsh7th/cmp-buffer'            -- Buffer completions
-  use 'hrsh7th/cmp-path'              -- Path completions
-  use 'hrsh7th/cmp-cmdline'           -- Command-line completions
-  use 'saadparwaiz1/cmp_luasnip'      -- Snippet completions
-  use 'L3MON4D3/LuaSnip'              -- Snippets plugin
+  'hrsh7th/nvim-cmp',              -- Autocompletion plugin
+  'hrsh7th/cmp-nvim-lsp',          -- LSP source for nvim-cmp
+  'hrsh7th/cmp-buffer',            -- Buffer completions
+  'hrsh7th/cmp-path',              -- Path completions
+  'hrsh7th/cmp-cmdline',           -- Command-line completions
+  'saadparwaiz1/cmp_luasnip',      -- Snippet completions
+  'L3MON4D3/LuaSnip',              -- Snippets plugin
   -- Telescope
-  use 'nvim-lua/plenary.nvim'         -- Lua functions used by various plugins
-  use 'nvim-telescope/telescope.nvim' -- Fuzzy finder and more
+  'nvim-lua/plenary.nvim',         -- Lua functions used by various plugins
+  'nvim-telescope/telescope.nvim', -- Fuzzy finder and more
 
-  use 'tpope/vim-fugitive'
+  'tpope/vim-fugitive',
 
   --editing
   --put back surround
   -- theme
-  use "rebelot/kanagawa.nvim"
+  "rebelot/kanagawa.nvim",
   -- bindings
-  use {
+  {
     "folke/which-key.nvim",
-    config = function()
-      require("which-key").setup {
-      }
-    end
-  }
-end)
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 50
+    end,
+    opts = {}
+  },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+})
 
+
+vim.g.lazydev_enabled = true
 
 -- theme
 vim.cmd('colorscheme kanagawa')
@@ -60,7 +87,6 @@ vim.bo.softtabstop = 2
 -- vim.g.python3_host_prog = vim.fn.system('pdm info --python').gsub('%s+', '')
 
 -- Basic keymaps
-vim.g.mapleader = " "
 local wk = require("which-key")
 wk.register({
   f = {
@@ -138,9 +164,4 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   callback = function()
     vim.cmd([[!latexmk -pdf -pv -xelatex]])
   end,
-})
-
-vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
-  pattern = 'init.lua',
-  command = "PackerInstall"
 })
