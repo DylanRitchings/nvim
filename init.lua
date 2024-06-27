de1e2bc1cab6ca0217f10e35373ac95f464a37fc
DylanRitchings <dylanritchings1998@gmail.com>
2024-06-27 15:33:13 +0100

    use more lazy vim

--- init.lua
+++ init.lua
@@ -11,106 +11,79 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	})
 end
 
-
 vim.opt.rtp:prepend(lazypath)
 --
 
 vim.g.mapleader = " "
 
 require("lazy").setup({
-	-- LSP Configuration & Plugins
-	'neovim/nvim-lspconfig', -- Collection of configurations for built-in LSP client
-	'williamboman/mason.nvim', -- LSP server installer
-	'stsewd/isort.nvim',
-	'nvim-treesitter/nvim-treesitter',
-	{
-		'nvim-treesitter/nvim-treesitter-textobjects',
-		dependencies = 'nvim-treesitter/nvim-treesitter',
-		after = 'nvim-treesitter/nvim-treesitter',
-		config = function()
-			require 'nvim-treesitter.configs'.setup {
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-							["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-						},
-						selection_modes = {
-							['@parameter.outer'] = 'v', -- charwise
-							['@function.outer'] = 'V', -- linewise
-							['@class.outer'] = '<c-v>', -- blockwise
-						},
-						include_surrounding_whitespace = true,
-					},
-					swap = {
-						enable = true,
-						swap_next = {
-							["<leader>a"] = "@parameter.inner",
-						},
-						swap_previous = {
-							["<leader>A"] = "@parameter.inner",
-						},
-					},
-					select = {
-						enable = true,
-						keymaps = {
-							-- Your custom capture.
-							["aF"] = "@custom_capture",
-
-							-- Built-in captures.
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-						},
-					},
-				},
-			}
-		end
+	spec = {
+		{
+			"LazyVim/LazyVim",
+			import = "lazyvim.plugins",
+			opts = {
+				colorscheme = "tokyonight",
+			},
+		},
+		{ import = "lazyvim.plugins.extras.dap.core" },
+		{ import = "lazyvim.plugins.extras.test.core" },
+		{ import = "lazyvim.plugins.extras.lang.python" },
+		{ import = "plugins" },
 	},
 
+	-- LSP Configuration & Plugins
+	"neovim/nvim-lspconfig", -- Collection of configurations for built-in LSP client
+	"williamboman/mason.nvim", -- LSP server installer
+	"stsewd/isort.nvim",
+	"nvim-treesitter/nvim-treesitter",
+
 	"elentok/format-on-save.nvim",
-	'mfussenegger/nvim-dap',
-	'folke/lazydev.nvim',
+
+	"folke/lazydev.nvim",
+	-- 'mfussenegger/nvim-dap-python',
 	"sharkdp/fd",
 	{
 		"X3eRo0/dired.nvim",
 		dependencies = "MunifTanjim/nui.nvim",
 		config = function()
-			require("dired").setup {
+			require("dired").setup({
 				path_separator = "/",
 				show_banner = false,
 				show_icons = false,
 				show_hidden = true,
 				show_dot_dirs = true,
 				show_colors = true,
-			}
-		end
+			})
+		end,
 	},
 	-- Autocompletion plugins
-	'hrsh7th/nvim-cmp',  -- Autocompletion plugin
-	'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
-	'hrsh7th/cmp-buffer', -- Buffer completions
-	'hrsh7th/cmp-path',  -- Path completions
-	'hrsh7th/cmp-cmdline', -- Command-line completions
-	'saadparwaiz1/cmp_luasnip', -- Snippet completions
-	'L3MON4D3/LuaSnip',  -- Snippets plugin
+  {
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+  },
+  },
+	"hrsh7th/cmp-buffer", -- Buffer completions
+	"hrsh7th/cmp-path", -- Path completions
+	"hrsh7th/cmp-cmdline", -- Command-line completions
+	"saadparwaiz1/cmp_luasnip", -- Snippet completions
+	"L3MON4D3/LuaSnip", -- Snippets plugin
 
 	-- Telescope
-	'nvim-lua/plenary.nvim', -- Lua functions used by various plugins
-	'ahmedkhalf/project.nvim',
+	"nvim-lua/plenary.nvim", -- Lua functions used by various plugins
+	"ahmedkhalf/project.nvim",
 	{
-		'nvim-telescope/telescope.nvim', -- Fuzzy finder and more
+		"nvim-telescope/telescope.nvim", -- Fuzzy finder and more
 		-- config = function()
 		--   require("telescope.builtin").find_files { path_display = { "truncate" } }
 		-- end
 	},
 
-
-	'tpope/vim-fugitive',
+	"tpope/vim-fugitive",
 
 	--editing
 	--put back surround\
@@ -118,18 +91,9 @@ require("lazy").setup({
 	-- Looks
 	-- "rebelot/kanagawa.nvim", -- Theme
 	{
-		-- Theme inspired by Atom
-		'navarasu/onedark.nvim',
-		priority = 1000,
-		lazy = false,
-		config = function()
-			require('onedark').setup {
-				-- Set a style preset. 'dark' is default.
-				style = 'light', -- dark, darker, cool, deep, warm, warmer, light
-				transparent = false
-			}
-			require('onedark').load()
-		end,
+		"folke/tokyonight.nvim",
+		lazy = true,
+		opts = { style = "day" },
 	},
 
 	-- bindings
@@ -140,7 +104,7 @@ require("lazy").setup({
 			vim.o.timeout = true
 			vim.o.timeoutlen = 50
 		end,
-		opts = {}
+		opts = {},
 	},
 	{
 		"folke/lazydev.nvim",
@@ -155,13 +119,12 @@ require("lazy").setup({
 	},
 })
 
-if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
-	vim.o.shell = '/c/Users/dylan.ritchings/dev/software/Git/usr/bin/bash.exe'
+
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+	vim.o.shell = "/c/Users/dylan.ritchings/dev/software/Git/usr/bin/bash.exe"
 end
 
 vim.g.lazydev_enabled = true
-
-
 
 -- Basic settings
 vim.o.timeout = true
@@ -184,16 +147,21 @@ local function git_add_commit_push()
 		print("Aborting commit due to empty commit message")
 		return
 	end
-	vim.cmd(':Git add -u')
+	vim.cmd(":Git add -u")
 	vim.cmd(':Git commit -m "' .. commit_message .. '"')
-	vim.cmd(':Git push')
+	vim.cmd(":Git push")
 end
 
 local function ex_in_dir(func)
 	return function()
-		func({ cwd = vim.fn.expand('%:p:h') })
+		func({ cwd = vim.fn.expand("%:p:h") })
 	end
 end
+
+-- local dap_py = require("dap-python")
+
+-- dap_py.setup("python")
+
 -- Basic keymaps/bindings
 local wk = require("which-key")
 local ts = require("telescope.builtin")
@@ -205,8 +173,8 @@ wk.register({
 		-- f = { "<cmd>Telescope find_files<cr>", "Search current dir" },
 		g = { ex_in_dir(ts.live_grep), "Grep" },
 		-- b = { "<cmd>Telescope buffers<cr>", "Buffers" },
-		d = { "<cmd>Dired<cr>", "Dired" },
-		c = { "<cmd>edit " .. vim.fn.stdpath('config') .. "/init.lua<cr>", "Config" },
+		d = { ":Dired<cr>", "Dired" },
+		c = { "<cmd>edit " .. vim.fn.stdpath("config") .. "/init.lua<cr>", "Config" },
 	},
 	g = {
 		name = "Git",
@@ -225,7 +193,6 @@ wk.register({
 		d = { ":bd<cr>", "Close" },
 	},
 
-
 	-- <cmd>lua ts.find_files({cwd = vim.fn.expand('%:p:h')})<cr>
 	-- TODO
 	-- - window movement, vert, hori, close...
@@ -233,33 +200,32 @@ wk.register({
 	-- - more file finding, current repo...
 	-- - git add this file, quick push remember last commit
 	-- telescope help
-
 }, { prefix = "<leader>" })
 
-require('telescope').setup {
+require("telescope").setup({
 	defaults = {
-		path_display = { "smart" }
-	}
-}
+		path_display = { "smart" },
+	},
+})
 -- LSP settings
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 
 -- Configure LSP
-local on_attach = function(client, bufnr)
-  if client.name == 'ruff' then
-    -- Disable hover in favor of Pyright
-    client.server_capabilities.hoverProvider = false
-  end
-end
+-- local on_attach = function(client, bufnr)
+-- 	if client.name == "ruff" then
+-- 		-- Disable hover in favor of Pyright
+-- 		client.server_capabilities.hoverProvider = false
+-- 	end
+-- end
 
-require('lspconfig').ruff.setup {
-  on_attach = on_attach,
-}
+-- require("lspconfig").ruff.setup({
+-- 	on_attach = on_attach,
+-- })
 
-lspconfig.lua_ls.setup {}
+lspconfig.lua_ls.setup({})
 
 -- Setup lspconfig with nvim-cmp capabilities
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+--local capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- lspconfig.pyright.setup {
 -- 	cmd = { "pyright-langserver", "--stdio" },
 -- 	capabilities = capabilities,
@@ -269,9 +235,33 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 --
 -- }
 -- vim.lsp.set_log_level("debug")
-lspconfig.pylsp.setup {}
-local cmp = require('cmp')
-local luasnip = require('luasnip')
+lspconfig.pylsp.setup({
+
+	settings = {
+		pylsp = {
+			plugins = {
+				-- formatter options
+				black = { enabled = true },
+				autopep8 = { enabled = false },
+				yapf = { enabled = false },
+				-- linter options
+				-- pylint = { enabled = true, executable = "pylint" },
+				ruff = { enabled = true },
+				pyflakes = { enabled = false },
+				pycodestyle = { enabled = false },
+				-- type checker
+				pylsp_mypy = { enabled = true },
+				-- auto-completion options
+				jedi_completion = { fuzzy = true },
+				-- import sorting
+				pyls_isort = { enabled = true },
+			},
+		},
+	},
+})
+
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 -- format on save
 local format_on_save = require("format-on-save")
@@ -296,36 +286,34 @@ cmp.setup({
 		end,
 	},
 	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
 	}),
 	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		{ name = 'luasnip' },
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
 	}, {
-		{ name = 'buffer' },
-	})
+		{ name = "buffer" },
+	}),
 })
 
 -- compile latex on save
-vim.api.nvim_create_augroup('AutoPdf', {})
-vim.api.nvim_create_autocmd('BufWritePost', {
-	pattern = '*.tex',
-	group = 'AutoPdf',
+vim.api.nvim_create_augroup("AutoPdf", {})
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = "*.tex",
+	group = "AutoPdf",
 	callback = function()
 		vim.cmd([[!latexmk -pdf -pv -xelatex]])
 	end,
 })
 
-vim.fn.system('pdm info --packages')
-
-
+vim.fn.system("pdm info --packages")
 
 -- Scroll down half a screen and keep cursor in the middle
-vim.api.nvim_set_keymap('n', '<C-D>', '<C-D>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-D>", "<C-D>zz", { noremap = true, silent = true })
 
 -- Scroll up half a screen and keep cursor in the middle
-vim.api.nvim_set_keymap('n', '<C-U>', '<C-U>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-U>", "<C-U>zz", { noremap = true, silent = true })
